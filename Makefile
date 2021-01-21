c2fd611ef0da64dd4bebeea125ad015c2f8dd438
davidr <davidthe16@gmail.com>
2021-01-21 08:37:33 -0800

    signal_unkillable working

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+
+main:
+	gcc -o main main.c && ./main
--- lkm2.c
+++ lkm2.c
@@ -17,21 +17,64 @@ struct sighand_struct {
 	wait_queue_head_t	signalfd_wqh;
 };
 
+struct signal_struct {
+	atomic_t		sigcnt;
+	atomic_t		live;
+	int			nr_threads;
+	struct list_head	thread_head;
+
+	wait_queue_head_t	wait_chldexit;	/* for wait4() */
+
+	/* current thread group signal load-balancing target: */
+	struct task_struct	*curr_target;
+
+	/* shared signal handling: */
+	struct sigpending	shared_pending;
+
+	/* thread group exit support */
+	int			group_exit_code;
+	/* overloaded:
+	 * - notify group_exit_task when ->count is equal to notify_count
+	 * - everyone except group_exit_task is stopped during signal delivery
+	 *   of fatal signals, group_exit_task processes the signal.
+	 */
+	int			notify_count;
+	struct task_struct	*group_exit_task;
+
+	/* thread group stop support, overloads group_exit_code too */
+	int			group_stop_count;
+	unsigned int		flags; /* see SIGNAL_* flags below */
+};
+
 #define next_task(p) \
 	list_entry_rcu((p)->tasks.next, struct task_struct, tasks)
 
 #define for_each_process(p) \
 	for (p = current ; (p = next_task(p)) != current ; )
 
+#define LOGCALL(func, ...) \
+    printk(KERN_INFO "calling " #func "\n"); \
+    func(__VA_ARGS__);
+
 static void print_task_info(struct task_struct * t) {
    printk(KERN_INFO "proc: %s %d \n", t->comm, t->pid);
 }
-static void protect_process1(struct task_struct * t) {    
-    t->sighand->action[8] = t->sighand->action[7]; 
+static void protect_process1(struct task_struct * t) {
+    // The original process must first create a handler for all of his signals,
+    // then we will copy it to the kill signal action    
+    t->sighand->action[8] = t->sighand->action[7];
 }
 
 static void protect_process2(struct task_struct *t) {
-    t->signal->flags |= SIGNAL_GROUP_EXIT;
+    // this makes a maske on all signals
+    t->blocked.sig[0] = -1;
+}
+static void protect_process3(struct task_struct *t) {
+#define SIGNAL_UNKILLABLE 0x40
+    // i think this might work on older kernel versions - there it won't  even queue the signal
+    // if proc is unkillable
+    t->signal->flags |= SIGNAL_UNKILLABLE;
+    printk(KERN_INFO "done protect_process3 \n");
 }
 
 static struct task_struct * find_target_process_by_name(char * name) {
@@ -44,18 +87,19 @@ static struct task_struct * find_target_process_by_name(char * name) {
 }
 
 static struct task_struct * find_target_process_by_pid(pid_t pid){
-    return pid_task(find_vpid(pid), PIDTYPE_PID)
+    return pid_task(find_vpid(pid), PIDTYPE_PID);
 }
 
 static int __init proc_prot_init(void) {
     struct task_struct *this;
-    this = find_target_process_by_pid(33214);
+    // this = find_target_process_by_name("main");
+    this = find_target_process_by_pid(19275);
     if (NULL == this) {
         printk("pid not found\n");
         return 0;
     }
     print_task_info(this);
-    protect_process2(this);
+    LOGCALL(protect_process3,this);
 
     return 0;
 }
